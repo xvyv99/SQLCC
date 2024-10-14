3dee3f12c4307efc68e980d3c078aa0280e821b5
xvyv99 <xvyv99@163.com>
2024-10-14 17:36:28 +0800

    .

--- test/test_sql.cc
+++ test/test_sql.cc
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(SQLTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+namespace {
+
+Class SQLTest : public testing::Test {
+	protected:
+		void SetUp() override {
+			// pass
+		}
+
+};	
+
 }
