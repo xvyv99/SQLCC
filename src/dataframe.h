1031bfe644d8c1a31c86ca48d3adea5234ea803c
xvyv99 <xvyv99@163.com>
2024-10-22 00:30:02 +0800

    modified:   src/dataframe.h

--- src/dataframe.h
+++ src/dataframe.h
@@ -1,9 +1,14 @@
 #ifndef _DATAFRAME_H
 #define _DATAFRAME_H
 
+#include <string>
+#include <vector>
+
+template <typename T>
 class DataFrame {
 private:
-    /* data */
+    std::vector<std::vector<T>> data_;
+    std::vector<std::string> col_name_;
 public:
     DataFrame(/* args */);
     ~DataFrame();
