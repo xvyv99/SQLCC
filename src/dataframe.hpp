9dd90cab6595308a20ac0fc78b2d622d6b128e41
xvyv99 <xvyv99@163.com>
2024-10-22 16:03:19 +0800

    Finish dataframe.hpp .

---
+++ src/dataframe.hpp
@@ -0,0 +1,123 @@
+#ifndef _DATAFRAME_H
+#define _DATAFRAME_H
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <optional>
+#include <string_view>
+#include <unordered_set>
+
+using df_shape = std::pair<std::size_t, std::size_t>;
+using index_name = std::vector<std::string>;
+
+template <typename T>
+class DataFrame {
+private:
+    std::vector<std::vector<T>> data_;
+    index_name col_names_;
+    df_shape shape_;
+public:
+    DataFrame();
+    DataFrame(index_name);
+    ~DataFrame();
+
+    df_shape shape();
+    bool hasValue();
+    bool setColN(index_name);
+
+    bool addRow(std::vector<T>);
+    // void addCol(std::vector<T> col);
+
+    std::optional<T> access(std::size_t, std::string_view);
+    std::optional<T> access(std::size_t, std::size_t);
+
+    std::optional<T> operator()(std::size_t, std::string_view);
+    std::optional<T> operator()(std::size_t, std::size_t);
+};
+
+template <typename T>
+DataFrame<T>::DataFrame() {
+    this->data_ = std::vector<std::vector<T>>();
+    this->shape_.first = 0;
+    this->shape_.second = 0;
+}
+
+template <typename T>
+DataFrame<T>::DataFrame(index_name coln) {
+    this->data_ = std::vector<std::vector<T>>();
+    this->shape_.first = 0;
+    this->shape_.second = coln.size();
+    this->col_names_ = coln;
+}
+
+template <typename T>
+DataFrame<T>::~DataFrame() {}
+
+template <typename T>
+df_shape DataFrame<T>::shape() {
+    return this->shape_;
+}
+
+template <typename T>
+bool DataFrame<T>::hasValue() {
+    return this->data_.size()!=0;
+}
+
+template <typename T>
+bool DataFrame<T>::setColN(index_name coln) {
+    if ((this->shape_.second!=coln.size() && this->shape_.second>0) || coln.size()==0)
+        return false; 
+    std::unordered_set<std::string> tmp_set(coln.begin(), coln.end());
+    if (tmp_set.size()!=coln.size())
+        return false;
+    if (this->shape_.second==0)
+        this->shape_.second = coln.size();
+    this->col_names_ = coln;
+    return true;
+}
+
+template <typename T>
+bool DataFrame<T>::addRow(std::vector<T> row) {
+    if (row.size()!=this->shape_.second) {
+        return false;
+    } else {
+        this->data_.push_back(row);
+        this->shape_.first += 1;
+        return true;
+    }
+}
+
+template <typename T>
+std::optional<T> DataFrame<T>::access(std::size_t row_num, std::string_view col_name) {
+    if (row_num>=this->shape().first) 
+        return std::nullopt;
+    auto it = std::find(this->col_names_.begin(), this->col_names_.end(), col_name);
+	if (it!=this->col_names_.end()) {
+		std::size_t index = std::distance(this->col_names_.begin(), it);
+		return this->data_[row_num][index];
+	} else {
+		return std::nullopt;
+	}
+}
+
+template <typename T>
+std::optional<T> DataFrame<T>::access(std::size_t row_num, std::size_t col_num) {
+    if (row_num<this->shape().first && col_num<this->shape().second) {
+        return this->data_[row_num][col_num];
+    } else {
+        return std::nullopt;
+    }
+}
+
+template <typename T>
+std::optional<T> DataFrame<T>::operator()(std::size_t row_num, std::string_view col_name) {
+    return this->access(row_num, col_name);
+};
+
+template <typename T>
+std::optional<T> DataFrame<T>::operator()(std::size_t row_num, std::size_t col_num) {
+    return this->access(row_num, col_num);
+};
+
+#endif
--- test/test_df.cc
+++ test/test_df.cc
@@ -1,10 +1,11 @@
-#include "../src/dataframe.h"
+#include "../src/dataframe.hpp"
 #include <gtest/gtest.h>
 
 TEST(DFtest, df1) {
     DataFrame<int> df1{};
     EXPECT_TRUE(df1.setColN({"abc", "def", "ghi"}));
     EXPECT_TRUE(df1.setColN({"ac", "def", "ghi"}));
+    EXPECT_FALSE(df1.setColN({"ac", "ac", "ac"}));
     EXPECT_FALSE(df1.setColN({"abc", "def", "ghi", "kln"}));
     EXPECT_FALSE(df1.setColN({"abc", "kln"}));
     EXPECT_FALSE(df1.hasValue());
@@ -15,4 +16,49 @@ TEST(DFtest, df1) {
     for (int i=0;i<50;i++)
         EXPECT_TRUE(df1.addRow({i+1, i+2, i+3}));
     EXPECT_EQ(df1.shape(), std::make_pair(51, 3));
+
+    EXPECT_EQ(df1.access(0,2), 3);
+    EXPECT_EQ(df1.access(26,1), 27);
+    EXPECT_EQ(df1.access(27,0), 27);
+    EXPECT_EQ(df1.access(0,"ghi"), 3);
+    EXPECT_EQ(df1.access(26,"def"), 27);
+    EXPECT_EQ(df1.access(27,"ac"), 27);
+
+    EXPECT_EQ(df1(0,2), 3);
+    EXPECT_EQ(df1(26,1), 27);
+    EXPECT_EQ(df1(27,0), 27);
+    EXPECT_EQ(df1(0,"ghi"), 3);
+    EXPECT_EQ(df1(26,"def"), 27);
+    EXPECT_EQ(df1(27,"ac"), 27);
+}
+
+TEST(DFtest, df2) {
+    DataFrame<std::string> df2{};
+    EXPECT_TRUE(df2.setColN({"abc", "def", "ghi", "g++", "gcc", "clang", "msvc"}));
+    EXPECT_TRUE(df2.setColN({"ac", "def", "ghi", "gcc", "g++", "clang", "msvc"}));
+    EXPECT_FALSE(df2.setColN({"ac", "def", "ghi", "g++", "g++", "clang", "msvc"}));
+    EXPECT_FALSE(df2.setColN({"abc", "def", "ghi", "kln"}));
+    EXPECT_FALSE(df2.setColN({"abc", "kln"}));
+    EXPECT_FALSE(df2.hasValue());
+    EXPECT_EQ(df2.shape(), std::make_pair(0, 7));
+
+    EXPECT_TRUE(df2.addRow({"Hello", "World", "Exec", "Hello", "World", "Exec", "Exec"}));
+    EXPECT_FALSE(df2.addRow({"What", "up"}));
+    for (int i=0;i<50;i++)
+        EXPECT_TRUE(df2.addRow({"1", "2", "3", "4", "5", "6", "7"}));
+    EXPECT_EQ(df2.shape(), std::make_pair(51, 7));
+
+    EXPECT_EQ(df2.access(50,6), "7");
+    EXPECT_EQ(df2.access(23,1), "2");
+    EXPECT_EQ(df2.access(22,0), "1");
+    EXPECT_EQ(df2.access(50,"msvc"), "7");
+    EXPECT_EQ(df2.access(23,"def"), "2");
+    EXPECT_EQ(df2.access(22,"ac"), "1");
+
+    EXPECT_EQ(df2(50,6), "7");
+    EXPECT_EQ(df2(23,1), "2");
+    EXPECT_EQ(df2(22,0), "1");
+    EXPECT_EQ(df2(50,"msvc"), "7");
+    EXPECT_EQ(df2(23,"def"), "2");
+    EXPECT_EQ(df2(22,"ac"), "1");
 }
